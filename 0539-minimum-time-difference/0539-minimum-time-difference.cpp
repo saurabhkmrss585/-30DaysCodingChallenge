3109466460607c903a214826211a1c071c6599c2
saurabhkmrss585 <125881417+saurabhkmrss585@users.noreply.github.com>
2025-03-01 07:14:28 +0530

    Time: 3 ms (73.35%), Space: 19.1 MB (17.49%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    
-    int minutes(string &s){
-        int hr=stoi(s.substr(0,2));
-        int min=stoi(s.substr(3,2));
-        int totaltime=hr*60+min;
-        return totaltime;
-    }
-    
-    
+
+   int convertToMin(string&hour){
+      int hr=stoi(hour.substr(0,2));
+      int min=stoi(hour.substr(3,2));
+      return hr*60+min;
+   }
+
+
     int findMinDifference(vector<string>& timePoints) {
-         vector <int>diff;
-         for(string s:timePoints){
-            diff.push_back(minutes(s));
-         }
-         sort(diff.begin(),diff.end());
-         int n=INT_MAX;
-         for(int i=0;i<diff.size()-1;i++){
-            n=min(n,diff[i+1]-diff[i]);
-            
-         }
-         n=min(n,diff[0]+1440-diff[diff.size()-1]);
-         return n;
+        vector<int>minutes;
+        for(auto time:timePoints){
+            minutes.push_back(convertToMin(time));
+        }
+        sort(minutes.begin(),minutes.end());
+        int ans=INT_MAX;
+       for(int i=0;i<minutes.size()-1;i++){
+       ans=min(ans,minutes[i+1]-minutes[i]);
+       }
+      int lastDiff=+minutes[0]+1440-minutes[minutes.size()-1];
+      ans=min(ans,lastDiff);
+      return ans;
+
     }
 };
